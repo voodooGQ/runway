d779da37873370475bb516f4629cccf6963aff6c
Shane Smith <voodoogq@gmail.com>
2020-04-06 08:33:39 -0500

    Basic cleanup

--- e2e/runner.py
+++ e2e/runner.py
@@ -1,11 +1,20 @@
 import os
+import logging
+
+from send2trash import send2trash
+
 from integration_tests.util import (copy_file, copy_dir)
+
+LOGGER = logging.getLogger(__name__)
 
 
 class Runner(object):
     base_dir = os.path.abspath(os.path.dirname(__file__))
     fixtures_dir = os.path.join(base_dir, 'fixtures')
     temp_dir = os.path.join(base_dir, 'tmp')
+
+    fixture_dir = ''
+    runway_file = ''
 
     def __init__(self, name="Test Runner"):
         self.name = name
@@ -16,10 +25,24 @@ class Runner(object):
             os.path.join(self.fixtures_dir, name),
             os.path.join(self.temp_dir, name)
         )
+        self.fixture_dir = os.path.join(self.temp_dir, name)
 
     def copy_runway(self, template):
         """Copy runway template to proper directory."""
         template_file = os.path.join(
             self.fixtures_dir, 'runway-{}.yml'.format(template)
         )
-        copy_file(template_file, os.path.join(self.temp_dir, 'runway.yml'))
+        copy_file(
+            template_file,
+            os.path.join(self.temp_dir, 'runway.yml')
+        )
+        self.runway_file = os.path.join(self.temp_dir, 'runway.yml')
+
+    def clean(self):
+        if os.path.isdir(self.fixture_dir):
+            LOGGER.debug('send2trash: "%s"', self.fixture_dir)
+            send2trash(self.fixture_dir)
+        if os.path.isfile(self.runway_file):
+            LOGGER.debug('send2trash: "%s"', self.runway_file)
+            send2trash(self.runway_file)
+
--- e2e/tests/auth_at_edge/test_login.py
+++ e2e/tests/auth_at_edge/test_login.py
@@ -3,14 +3,18 @@ from requests import get
 from ...pages.auth_at_edge.login_page import LoginPage
 from ...runner import Runner
 
+
 @pytest.fixture(scope="module", autouse=True)
 def setup(driver):
     global loginPage
+    global runner
     runner = Runner('Test Login')
     runner.copy_fixture('e2e-auth-at-edge')
     runner.copy_runway('auth-at-edge')
     loginPage = LoginPage(driver, 'https://shaneallensmith.com')
     loginPage.goto('/')
+    yield
+    runner.clean()
 
 def test_login_returns_status_200(driver):
     res = get(loginPage.get_base_url())
